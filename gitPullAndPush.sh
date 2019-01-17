3dc2304170cb07d0b2d0c822db89bbc0487dc571
zhanghk <zhanghk@asiainfo-sec.com>
2019-01-17 18:55:54 +0800

    add git script

---
+++ gitPullAndPush.sh
@@ -0,0 +1,9 @@
+echo --------------git stash-------------------
+git stash
+
+echo --------------git pull --rebase-------------------
+git pull --rebase
+echo --------------git push-------------------
+git push
+echo --------------git stash pop-------------------
+git stash pop
